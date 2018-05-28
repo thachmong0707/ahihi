3913fbb88941e249f38aa894ceb82dea02f287c7
DESKTOP-H184TC7\Vo Huyen Lan Uyen <lanuyen.uit@gmail.com>
2018-05-28 22:21:49 +0700

    load avatar of user from database

--- vendor/js/modal.js
+++ vendor/js/modal.js
@@ -10,6 +10,13 @@
       $("#username").text($(tds[2]).text());
       $("#user-role").text($(tds[3]).text());
       $("#user-info").text($(tds[4]).text());
+      var picture = $(tds[5]).text();
+      if(picture === ""){
+        $("#user-avatar").prop("src","../../vendor/img/avatar.png");
+      }else{
+        $("#user-avatar").prop("src","../../vendor/img/"+picture);
+
+      }
       $("#userDetailModal").modal();
       
     }); 
