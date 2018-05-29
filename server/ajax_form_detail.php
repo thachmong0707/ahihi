a12678b8c4c32c0b34dc6b451463830a3b6340a0
tranchinamitytb@gmail.com <nam.tran@beesightsoft.com>
2018-05-30 01:36:03 +0700

    fix

--- server/ajax_form_detail.php
+++ server/ajax_form_detail.php
@@ -13,24 +13,34 @@
 			
 		}
 		// var_dump($oneObj[0]);
+
+		//get cusID from form
 		$formId = 1;
         $accountType =$oneObj[0] ;
         $accountType = substr($accountType, 1);;
-        var_dump($accountType);
+        // var_dump($accountType);
         $money = $oneObj[4];
         $moneyVND = $oneObj[5];
 		$dateCreated = date("y-m-d");
         $contractType = 1;
+        $customerId = $oneObj[7];
+
+
+// print_r($formId);
+// print_r($accountType);
+// print_r($money);
+// print_r($moneyVND);
+// print_r($dateCreated);
 
 
         $sql = "INSERT INTO form_detail
-            VALUES ( '".$formId."', '".$accountType."', '".$money."', '".$moneyVND."', '".$dateCreated."', '".$contractType."')";
+            VALUES ( '".$formId."', '".$accountType."', '".$money."', '".$moneyVND."', '".$dateCreated."', '".$contractType."', '".$customerId."')";
             $conn->query($sql);
-  //           if ($conn->query($sql) === TRUE) {
-		// 	    echo "New record created successfully";
-		// 	} else {
-		// 	    echo "Error: " . $sql . "<br>" . $conn->error;
-		// 	}
+   //          if ($conn->query($sql) === TRUE) {
+			//     echo "New record created successfully";
+			// } else {
+			//     echo "Error: " . $sql . "<br>" . $conn->error;
+			// }
 		// print_r($oneObj[0]);
 		// print_r($oneObj[1]);
 		// print_r($oneObj[2]);
