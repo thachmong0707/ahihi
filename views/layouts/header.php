4c14c8873b6c1ecce1d1cebd0b44f98fb1314bf7
QuyenLan <phancongquyen2010@gmail.com>
2018-05-30 21:29:55 +0700

    revert tai vi tri cua nam
    aff

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -31,7 +31,7 @@
 
     <link rel="stylesheet" type="text/css" href="../../vendor/css/view-css.css">
     <link rel="stylesheet" type="text/css" href="../../vendor/css/listUser.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
